66580f59f80fc6f6b7f8b69fd61ae5a8beaf247c
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-09-30 23:23:48 +0100

    feat: add inhouse shadcn library

---
+++ packages/ui/src/components/animated-theme-toggler.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+import { flushSync } from "react-dom";
+import { Button, ButtonProps } from "@workspace/ui/components/button";
+import { cn } from "@workspace/ui/lib/utils";
+
+type Props = {
+  className?: string;
+  iconClassName?: string;
+  size?: ButtonProps["size"];
+  variant?: ButtonProps["variant"];
+};
+
+export const AnimatedThemeToggler = ({
+  className,
+  iconClassName,
+  size = "sm",
+  variant = "outline",
+}: Props) => {
+  const [isDark, setIsDark] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const updateTheme = () => {
+      setIsDark(document.documentElement.classList.contains("dark"));
+    };
+
+    updateTheme();
+
+    const observer = new MutationObserver(updateTheme);
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  const toggleTheme = useCallback(async () => {
+    if (!buttonRef.current) return;
+
+    await document.startViewTransition(() => {
+      flushSync(() => {
+        const newTheme = !isDark;
+        setIsDark(newTheme);
+        document.documentElement.classList.toggle("dark");
+        localStorage.setItem("theme", newTheme ? "dark" : "light");
+      });
+    }).ready;
+
+    const { top, left, width, height } =
+      buttonRef.current.getBoundingClientRect();
+    const x = left + width / 2;
+    const y = top + height / 2;
+    const maxRadius = Math.hypot(
+      Math.max(left, window.innerWidth - left),
+      Math.max(top, window.innerHeight - top)
+    );
+
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${maxRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration: 700,
+        easing: "ease-in-out",
+        pseudoElement: "::view-transition-new(root)",
+      }
+    );
+  }, [isDark]);
+
+  return (
+    <Button
+      ref={buttonRef}
+      onClick={toggleTheme}
+      className={cn(className)}
+      variant={variant}
+      size={size}
+    >
+      {isDark ? (
+        <Sun className={cn(iconClassName)} />
+      ) : (
+        <Moon className={cn(iconClassName)} />
+      )}
+    </Button>
+  );
+};

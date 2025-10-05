fec5c9c68fdba161277ce1f893be8452705430ec
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-05 14:13:27 +0100

    feat: add app download page

---
+++ apps/web/app/help/download/page.tsx
@@ -0,0 +1,18 @@
+import PageSection from "@/components/page-section";
+
+export default function DownloadPage() {
+  return (
+    <main>
+      <PageSection>
+        <h1 className="text-3xl font-bold">How to Download</h1>
+      </PageSection>
+      <PageSection>
+        <p className="text-lg">
+          To download the Sports Management System application, please visit our
+          official website and navigate to the "Download" section. Follow the
+          instructions provided to ensure a smooth installation process.
+        </p>
+      </PageSection>
+    </main>
+  );
+}

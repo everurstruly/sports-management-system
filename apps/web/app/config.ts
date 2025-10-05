3effc5d5f1a90c2927a98854323a5c0f93035686
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-05 22:29:13 +0100

    fix: correct brand name and update social media links

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,12 +1,12 @@
 export const config = {
-  brandName: "Rena",
+  brandName: "Renna",
   appUrl:
     process.env.NODE_ENV === "production"
-      ? process.env.VERCEL_PROJECT_PRODUCTION_URL ??
-        process.env.NEXT_PUBLIC_APP_URL!
+      ? (process.env.VERCEL_PROJECT_PRODUCTION_URL ??
+        process.env.NEXT_PUBLIC_APP_URL!)
       : "localhost:3000",
   social: {
-    github: "https://github.com/akash3444/shadcn-ui-blocks",
-    twitter: "https://twitter.com/shadcnui_blocks",
+    github: "https://github.com/oghenetefa",
+    twitter: "https://twitter.com/oghenetefa",
   },
 };

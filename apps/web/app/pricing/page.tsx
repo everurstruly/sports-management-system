bb4c870eed87d471fc4a44b6fcc3170dddbb3e8d
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-05 14:13:12 +0100

    feat: add pricing page

---
+++ apps/web/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import PageSection from "@/components/page-section";
+import PricingSection from "@/app/pricing/pricing-section";
+
+export default function PricingPage() {
+  return (
+    <main>
+      {/* <PageSection>
+        <h1 className="text-3xl font-bold">Pricing Plans</h1>
+      </PageSection> */}
+      <PricingSection />
+    </main>
+  );
+}

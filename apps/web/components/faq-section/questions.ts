ba85cbfe938c0b128dc56fe28d35e6ce4a5b9e2d
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-06 08:41:06 +0100

    feat: update FAQ section with new questions and answers about Renna

--- apps/web/components/faq-section/questions.ts
+++ apps/web/components/faq-section/questions.ts
@@ -1,27 +1,89 @@
 export default [
   {
-    question: "What is your return policy?",
+    question: "Who is Renna for?",
     answer:
-      "You can return unused items in their original packaging within 30 days for a refund or exchange. Contact support for assistance.",
+      "Renna is for anyone organizing activities for other people. It can be a soccer coach, choir conductor, scout or swim coach. Renna makes together better!",
   },
   {
-    question: "How do I track my order?",
+    question: "What are the main benefits of using Renna?",
     answer:
-      "Track your order using the link provided in your confirmation email, or log into your account to view tracking details.",
+      "Renna simplifies scheduling, improves communication, and enhances participant engagement through features like real-time updates, easy sign-ups, and integrated messaging.",
   },
   {
-    question: "Do you ship internationally?",
+    question: "Does Renna cost money?",
     answer:
-      "Yes, we ship worldwide. Shipping fees and delivery times vary by location, and customs duties may apply for some countries.",
+      "Renna is free to download and use! The only cost is if you choose to collect money through Renna, then there will be a transaction fee. There are no hidden fees.",
   },
   {
-    question: "What payment methods do you accept?",
-    answer:
-      "We accept Visa, MasterCard, American Express, PayPal, Apple Pay, and Google Pay, ensuring secure payment options for all customers.",
+    question: "Do I need to download an app to use Renna?",
+    answer: `No, you can use Renna through a web browser or by downloading it as a browser-app on your phone. Checkout "How to Download" for instructions.`,
+  },
+  // {
+  //   question: "What is your business model?",
+  //   answer:
+  //     "Our primary goal is to make organizing activities easier for administrators, parents, and participants with our free-to-use app. Renna generates revenue from a transaction fee for those who choose to collect money for their teams through the app.",
+  // },
+  // {
+  //   question: "I want to learn more about Renna?",
+  //   answer:
+  //     "You can learn more about Renna by visiting our website at renna.netlify.app, where you'll find detailed information about our features, pricing, and user testimonials. Additionally, feel free to reach out to our support team for any specific questions or assistance.",
+  // },
+  {
+    question: "I am looking for commercial opportunities or partnerships",
+    answer: "Contact our support team here and our team will be in touch.",
   },
   {
-    question: "What if I receive a damaged item?",
+    question: "How do I get started as a host/staff of a tournament?",
+    answer: `
+      1. Create a Renna profile – Try it! It’s FREE
+      2. Go to your profile and click on ‘Groups’ followed by ‘Create group’
+      3. Enter a group name and optionally create subgroups
+      4. Specify the activity (football, choir, boating, etc.) and age group (children, adults or mixed) of your group.
+      5. Create a group description which will be presented to new members when they are invited
+      6. Click ‘Create group’ to finish setting up the group
+      7. Edit group settings such as custom member fields or admin roles
+      8. Invite members. Read more.
+      `,
+  },
+  {
+    question: "How do I become a member of a society?",
+    answer: `
+      A society leader can invite you by sharing a society link/code with you. You can also join public societies that require review but no invitation.
+
+      You have not received a society code?
+      1. Download the Renna app (It’s free!)
+      2. Register and create a profile in the app
+      3. In the “Groups” tab, accept the group invitation.
+
+      You have received a society code:
+      1. Download the app in App Store or Play Store (It’s free!)
+      2. Register and create a profile in the app
+      3. In the “Groups” tab, press the “Enter code” button near the bottom right corner and follow the instructions.
+      `,
+  },
+  {
+    question: "Can I synchronize Renna with my calendar?",
     answer:
-      "Please contact our support team within 48 hours of delivery with photos of the damaged item. We’ll arrange a replacement or refund.",
+      "Yes! This is very important, for our users managing a venue! You will be able to synchronize events with your calendar if you have the Renna app and you can change these settings within your profile.",
+  },
+  {
+    question: "What are Renna Brand Partnerships?",
+    answer:
+      "Renna brand partnerships are collaborations with carefully selected brands that help keep the Renna app free for its 1.4 million UK users while ensuring relevant and non-intrusive content for grassroots sports teams and organisers.",
+  },
+  {
+    question: "Does Renna offer advertising?",
+    answer:
+      "Yes, Renna offers advertising through its Brand Partnerships programme. However, this is not traditional digital advertising; instead, it is carefully integrated content that aligns with the interests of Renna’s user base.",
+  },
+  {
+    question: "Are Renna's Brand Partnerships the same as advertising?",
+    answer:
+      "While Renna brand partnerships include advertising elements, they are designed to be user-friendly and non-intrusive, differing from traditional digital ads. These partnerships prioritise relevant content that enhances the user experience.",
+  },
+  {
+    question: "How can I advertise on the Renna platform?",
+    answer:
+      "Brands interested in advertising through Renna brand partnerships can contact the Renna team via email at ads@renna.com.",
   },
 ];

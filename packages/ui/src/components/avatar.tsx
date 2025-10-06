5a9e37d5f4f9cb3e87c7f2f407c6127bd05377e0
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-06 07:58:13 +0100

    fix: remove style distorting shape

--- packages/ui/src/components/avatar.tsx
+++ packages/ui/src/components/avatar.tsx
@@ -42,7 +42,7 @@ function AvatarFallback({
     <AvatarPrimitive.Fallback
       data-slot="avatar-fallback"
       className={cn(
-        "bg-muted text-secondary-foreground font-medium shadow-sm flex size-full items-center justify-center rounded-full",
+        "bg-muted text-secondary-foreground font-medium flex size-full items-center justify-center rounded-full",
         className
       )}
       {...props}

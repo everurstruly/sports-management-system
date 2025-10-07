223c601c6c263815369c2c944891d956aa9c8144
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-07 03:03:53 +0100

    feat: simplify pricing section layout and remove unused components

--- apps/web/app/pricing/hero-section.tsx
+++ apps/web/app/pricing/hero-section.tsx
@@ -1,91 +1,23 @@
-import { Button } from "@workspace/ui/components/button";
-import { Check } from "lucide-react";
-import Link from "next/link";
 import PageSection from "@/components/page-section";
-import SectionHeading from "@/components/section-heading";
+import PageHeading from "@/components/page-heading";
+import FreePricingPlanCard from "./free-plan-card";
+import PremiumPricingPlanCard from "./premium-plan-card";
 
 export default function PricingSection() {
   return (
-    <PageSection className="relative py-18 lg:pb-20">
-      <header className="mx-auto max-w-2xl text-center mb-8 md:mb-20">
-        <SectionHeading className="mx-auto">
-          Simple, Transparent Pricing
-        </SectionHeading>
-      </header>
+    <>
+      <PageSection className="relative py-18">
+        <PageHeading className="mx-auto text-center">Pricing</PageHeading>
 
-      <div className="bg-card relative rounded-3xl border shadow-2xl shadow-zinc-950/5">
-        <div className="grid items-center gap-12 divide-y p-12 md:grid-cols-2 md:divide-x md:divide-y-0">
-          <div className="pb-12 text-center md:pb-0 md:pr-12">
-            <h3 className="text-2xl font-semibold">Suite Enterprise</h3>
-            <p className="mt-2 text-lg">For your company of any size</p>
-            <span className="mb-6 mt-12 inline-block text-6xl font-bold">
-              <span className="text-4xl">$</span>234
-            </span>
+        <div className="absolute inset-0 -z-1 bg-[repeating-linear-gradient(105deg,#eee_0px_1px,transparent_1px_8px)] dark:bg-[repeating-linear-gradient(105deg,#222_0px_1px,transparent_1px_8px)] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#555_70%,transparent_110%)]"></div>
+      </PageSection>
 
-            <div className="flex justify-center">
-              <Button asChild size="lg">
-                <Link href="#">Get started</Link>
-              </Button>
-            </div>
-
-            <p className="text-muted-foreground mt-12 text-sm">
-              Includes : Security, Unlimited Storage, Payment, Search engine,
-              and all features
-            </p>
-          </div>
-          <div className="relative">
-            <ul role="list" className="space-y-4">
-              {[
-                "First premium advantage",
-                "Second advantage weekly",
-                "Third advantage donate to project",
-                "Fourth, access to all components weekly",
-              ].map((item, index) => (
-                <li key={index} className="flex items-center gap-2">
-                  <Check className="size-3" />
-                  <span>{item}</span>
-                </li>
-              ))}
-            </ul>
-            <p className="text-muted-foreground mt-6 text-sm">
-              Team can be any size, and you can add or switch members as needed.
-              Companies using our platform include:
-            </p>
-            <div className="mt-12 flex flex-wrap items-center justify-between gap-6">
-              <img
-                className="h-5 w-fit dark:invert"
-                src="https://html.tailus.io/blocks/customers/nvidia.svg"
-                alt="Nvidia Logo"
-                height="20"
-                width="auto"
-              />
-              <img
-                className="h-4 w-fit dark:invert"
-                src="https://html.tailus.io/blocks/customers/column.svg"
-                alt="Column Logo"
-                height="16"
-                width="auto"
-              />
-              <img
-                className="h-4 w-fit dark:invert"
-                src="https://html.tailus.io/blocks/customers/github.svg"
-                alt="GitHub Logo"
-                height="16"
-                width="auto"
-              />
-              <img
-                className="h-5 w-fit dark:invert"
-                src="https://html.tailus.io/blocks/customers/nike.svg"
-                alt="Nike Logo"
-                height="20"
-                width="auto"
-              />
-            </div>
-          </div>
+      <PageSection className="!pt-4">
+        <div className="grid gap-y-20">
+          <FreePricingPlanCard />
+          {/* <PremiumPricingPlanCard /> */}
         </div>
-      </div>
-
-      <div className="absolute inset-0 -z-1 bg-[repeating-linear-gradient(105deg,#eee_0px_1px,transparent_1px_8px)] dark:bg-[repeating-linear-gradient(105deg,#222_0px_1px,transparent_1px_8px)] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#555_70%,transparent_110%)]"></div>
-    </PageSection>
+      </PageSection>
+    </>
   );
 }

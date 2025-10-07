939bf0f996c2ab82402d92a3d9f2f89d27a6ca4e
Oghenetefa Okotete <93017833+everurstruly@users.noreply.github.com>
2025-10-07 07:54:38 +0100

    fix: update broken import path

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -64,7 +64,7 @@ export const metadata: Metadata = {
     title,
     images: [
       {
-        url: "/og-image.png",
+        url: "/og-image.jpg",
         width: 1200,
         height: 630,
         alt: title,

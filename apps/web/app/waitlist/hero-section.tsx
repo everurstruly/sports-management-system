be70b3bece604abc99d6e00fcce870cbe66ad4d3
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-13 05:29:57 +0100

    fix: remove extra spaces for better layout

--- apps/web/app/waitlist/hero-section.tsx
+++ apps/web/app/waitlist/hero-section.tsx
@@ -14,7 +14,7 @@ import WaitlistForm from "./form";
 export default function WaitlistSection() {
   return (
     <PageSection className="relative flex items-center justify-center h-full overflow-hidden py-18 lg:pb-20">
-      <BackgroundLines className="container flex flex-col items-center justify-center w-full px-4 md:h-full">
+      <BackgroundLines className="container flex flex-col items-center justify-center w-full md:h-full">
         <PageHeading className="mx-auto text-center">
           Join the Waitlist
         </PageHeading>
@@ -22,7 +22,7 @@ export default function WaitlistSection() {
           Be the first to know when we launch. Sign up now to secure your spot!
         </PageSubheading>
 
-        <WaitlistForm className="relative z-20 flex items-start w-full max-w-md gap-3 p-1 mt-10" />
+        <WaitlistForm className="relative z-20 flex items-start w-full max-w-md gap-3 mt-10" />
       </BackgroundLines>
 
       <div className="absolute inset-0 -z-1 bg-[repeating-linear-gradient(105deg,#eee_0px_1px,transparent_1px_8px)] dark:bg-[repeating-linear-gradient(105deg,#222_0px_1px,transparent_1px_8px)] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#555_70%,transparent_110%)]"></div>

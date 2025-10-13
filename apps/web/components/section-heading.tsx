78aa325e699ee9c748b4b9f517281592877eef0a
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-13 15:56:47 +0100

    fix: ensure TextAnimate component runs animation only once

--- apps/web/components/section-heading.tsx
+++ apps/web/components/section-heading.tsx
@@ -14,6 +14,7 @@ export default function SectionHeading({
       animation="slideLeft"
       by="word"
       as="h2"
+      once
       className={`leading-tight text-[32px] min-[260px]:text-[46px] min-[260px]:leading-tight sm:text-6xl xl:text-7xl uppercase font-impact font-bold text-zinc-900 dark:text-zinc-100 mb-4 max-w-sm sm:max-w-xl lg:max-w-2xl ${className}`}
     >
       {children}
